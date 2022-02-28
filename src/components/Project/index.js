ce3278cc7393d0b5b6ebbcee099affa6d4be5cc0
Osama Dahnoun <omdahnou@ncsu.edu>
2022-02-27 22:01:55 -0500

    image overlay effect

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaGithub } from 'react-icons/fa';
+
 
 const Project = () => {
 
@@ -6,37 +8,52 @@ const Project = () => {
         {
             alt: 'CalisthenicsClub',
             href: 'https://tranquil-bastion-94254.herokuapp.com/',
+            text: 'Express.js / Handlebar.js / Sequelize',
+            github: 'https://github.com/Osamadahnoun/CalisthenicsClub'
         },
         {
-            alt: 'TechBlog',
+            alt: 'The Tech Blog',
             href: 'https://obscure-wildwood-61807.herokuapp.com/',
+            text: 'Express.js / Handlebar.js / Sequelize',
+            github: 'https://github.com/Osamadahnoun/Tech-Blog'
         },
         {
             alt: 'LyricLover',
             href: 'https://osamadahnoun.github.io/LyricLover/',
+            text: 'HTML / CSS (Bulma) / Javascript',
+            github: 'https://github.com/Osamadahnoun/LyricLover'
         },
         {
             alt: 'Photoport',
             href: 'https://osamadahnoun.github.io/photo-port/',
+            text: 'React.js',
+            github: 'https://github.com/Osamadahnoun/photo-port'
         },
         {
-            alt: 'NoSqlAPI',
+            alt: 'social-network-API',
             href: 'https://github.com/Osamadahnoun/NoSQL-SocialNetwork-API',
+            text: 'NoSQL(MongoDB) / Mongoose / Express.js',
+            github: 'https://github.com/Osamadahnoun/NoSQL-SocialNetwork-API'
         },
         {
-            alt: 'SqlAPI',
+            alt: 'e-commerce-backend',
             href: 'https://github.com/Osamadahnoun/e-commerce-backend',
+            text: 'MySQL / Sequelize / Express.js',
+            github: 'https://github.com/Osamadahnoun/e-commerce-backend'
         }
     ]
 
     return (
         <section className="gallery">
              {photos.map((photo, i) => (
-<<<<<<< HEAD
-                     <img src={require(`../../assets/images/${i}.png`)} alt={photo.alt} onClick={() => window.open(`${photo.href}`)}/>
-=======
-                    <img src={require(`../../assets/images/${i}.png`)} alt={photo.alt} onClick={() => window.open(`${photo.href}`)}/>
->>>>>>> main
+                 <div className="span">
+                    <img src={require(`../../assets/images/${i}.png`)} alt={photo.alt}/>
+                    <div className="image__overlay">
+                        <span className="image__title" onClick={() => window.open(`${photo.href}`)}>{photo.alt}</span>
+                        <span className="githubLink"><FaGithub onClick={() => window.open(`${photo.github}`)}/></span>
+                        <p className="image__description">{photo.text}</p>
+                    </div>
+                 </div>
         ))}
         </section>
     )
